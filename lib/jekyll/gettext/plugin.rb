31b6130cb62c266951861352b24fcee2ee33283b
Lucas Doyle <lukedoyle@mujin.co.jp>
2014-03-04 19:13:20 +0900

    draft support for writing back to a po file

--- lib/jekyll/gettext/plugin.rb
+++ lib/jekyll/gettext/plugin.rb
@@ -3,9 +3,25 @@ require "jekyll/gettext/plugin/version"
 require 'fast_gettext'
 require 'get_pomo'
 
-# require 'pry'
+require 'pry'
+
+class MissingTranslationLogger
+  def initialize
+    @translations = []
+  end
+  
+  def get_translations
+    return @translations
+  end
+  
+  def call(unfound)
+    @translations.push(unfound)
+  end
+end
+
 
 module Jekyll
+
   class Site
     
     alias :process_org :process
@@ -22,19 +38,22 @@ module Jekyll
 
       # loop
       self.config['lang'] = languages.first
-      self.load_translations
       puts
       puts "Building site for default language: \"#{self.config['lang']}\" to: " + self.dest
+      self.load_translations
       process_org
+      self.save_missing_translations
+      
       languages.drop(1).each do |lang|
 
         # build site for language lang
         self.dest = self.dest + "/" + lang
         self.config['baseurl'] = self.config['baseurl'] + "/" + lang
         self.config['lang'] = lang
-        self.load_translations
         puts "Building site for language: \"#{self.config['lang']}\" to: " + self.dest
+        self.load_translations
         process_org
+        self.save_missing_translations
 
         # reset variables for next language
         self.dest = dest_org
@@ -43,15 +62,38 @@ module Jekyll
       puts 'Build complete'
     end
 
-    # TODO:
-    # parse the po file and store it in site whenever lang changes
-    # if a key is missing, add it to a list on site object
-    # when site is done processing, write back to po file
-
     def load_translations
-      FastGettext.add_text_domain(self.config['lang'], :path => self.source + "/_i18n", :type => :po)
+      FastGettext.add_text_domain(self.config['lang'], :path => self.source + "/_i18n", :type => :po, :ignore_fuzzy => false)
       FastGettext.text_domain = self.config['lang']
       FastGettext.locale = self.config['lang']
+
+      @missing_translations = MissingTranslationLogger.new
+
+      repos = [
+        FastGettext::TranslationRepository.build('logger', :type=>:logger, :callback=>lambda{|_|}),
+        FastGettext::TranslationRepository.build('logger', :type=>:logger, :callback=>@missing_translations)
+      ]
+      FastGettext.add_text_domain(self.config['lang'], :type=>:chain, :chain=>repos)
+    end
+
+    def save_missing_translations
+      filename = self.source + "/_i18n/" + self.config['lang'] + '/' + self.config['lang'] + '.po'
+      existing_translations = GetPomo.unique_translations(GetPomo::PoFile.parse(File.read(filename)))
+      
+      # ignores any keys that already exist
+      missing_translations_msgids = @missing_translations.get_translations.reject{|msgid| existing_translations.find{|trans| trans.msgid == msgid}}
+      
+      new_translations = existing_translations
+      
+      missing_translations_msgids.each do |new_msgid|
+        new_trans = GetPomo::Translation.new
+        new_trans.msgid = new_msgid
+        new_trans.msgstr = ""
+        new_translations.push(new_trans)
+      end
+      new_translations.sort_by!(&:msgid)
+
+      File.open(filename + '.test', 'w'){|f|f.print(GetPomo::PoFile.to_text(new_translations))}
     end
   end
 
@@ -64,22 +106,7 @@ module Jekyll
     end
 
     def render(context)
-      if "#{context[@key]}" != "" # check for page variable
-        key = "#{context[@key]}"
-      else
-        key = @key
-      end
-      candidate = _(key)
-
-      # binding.pry
-
-      lang = context.registers[:site].config['lang']
-
-      if candidate == key and lang != 'en'
-        puts "Missing i18n key: " + lang + ":" + key
-        "*" + lang + ":" + key + "*"
-      end
-      candidate
+      return _(@key)
     end
   end
 end
